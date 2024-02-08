2b09aef988ecacfaa60cd7a4bdbec7305988119e
shaista1016 <shai.khan1016@gmail.com>
2024-02-08 08:56:50 +0530

    commit

---
+++ github/src/github/Movie.java
@@ -0,0 +1,14 @@
+package github;
+
+public class Movie {
+	private String title;
+private String director;
+   public void movie (String title,String director) {
+	   this.title=title;
+	   this.director=director;
+   }
+   public String getTitle() {
+	   return this.title;
+   }
+
+}
